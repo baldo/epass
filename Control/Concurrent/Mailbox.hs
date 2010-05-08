e37ba748995170b5fce30048387c976950ef3999
Andreas Baldeau <andreas@baldeau.net>
2010-05-08 13:16:15 +0200

    Some cleanup.

--- Control/Concurrent/Mailbox.hs
+++ Control/Concurrent/Mailbox.hs
@@ -19,9 +19,24 @@ import Prelude hiding (catch)
 
 import Control.Concurrent
 import Control.Exception
-import Control.Monad
 import Data.Time
 import System.Timeout
+
+-- Timeout calculations
+
+timeoutFactor :: Num a => a
+timeoutFactor = 1000000
+
+calcEndTime :: Int -> IO UTCTime
+calcEndTime to = do
+    curTime <- getCurrentTime
+    let dt = fromIntegral to / timeoutFactor
+    return $ addUTCTime dt curTime
+
+calcTimeLeft :: UTCTime -> IO Int
+calcTimeLeft endTime = do
+    curTime <- getCurrentTime
+    return $ round $ (diffUTCTime endTime curTime) * timeoutFactor
 
 newtype Mailbox m = MBox (Chan m)
 
@@ -41,18 +56,52 @@ send (MBox chan) = writeChan chan
 
 receive :: Mailbox m -> [MsgHandler m] -> IO ()
 receive _ [] = return ()
-receive (MBox chan) handlers = matchAll chan Nothing handlers
-
-receiveNonBlocking :: Mailbox m -> [MsgHandler m] -> IO ()
-receiveNonBlocking (MBox chan) handlers = matchCurrent chan handlers
+receive (MBox chan) handlers = matchAll chan handlers
 
 receiveTimeout :: Mailbox m -> Int -> [MsgHandler m] -> IO ()
 receiveTimeout _ _ [] = return ()
 receiveTimeout mbox 0 handlers = receiveNonBlocking mbox handlers
 receiveTimeout (MBox chan) to handlers = do
-    curTime <- getCurrentTime
-    let dt = fromIntegral to / 1000000
-    matchAll chan (Just $ addUTCTime dt curTime) handlers
+    endTime <- calcEndTime to
+    matchAllTimeout chan endTime handlers
+
+receiveNonBlocking :: Mailbox m -> [MsgHandler m] -> IO ()
+receiveNonBlocking (MBox chan) handlers = matchCurrent chan handlers
+
+matchAll :: Chan m -> [MsgHandler m] -> IO ()
+matchAll chan hs = do
+    m <- readChan chan
+    matched <- match m hs
+
+    if matched
+        then
+            return ()
+        else do
+            matchAll chan hs
+            unGetChan chan m
+
+matchAllTimeout :: Chan m -> UTCTime -> [MsgHandler m] -> IO ()
+matchAllTimeout chan endTime hs = do
+    timeLeft <- calcTimeLeft endTime
+
+    if timeLeft <= 0
+        then return ()
+        else do
+            mm <- timeout timeLeft $ readChan chan
+
+            case mm of
+                Just m -> do
+                    matched <- matchTimeout m endTime hs
+                    case matched of
+                        Just False -> do
+                            matchAllTimeout chan endTime hs
+                            unGetChan chan m
+                        Just True ->
+                            return ()
+                        Nothing ->
+                            unGetChan chan m
+                Nothing ->
+                    return ()
 
 matchCurrent :: Chan m -> [MsgHandler m] -> IO ()
 matchCurrent chan hs = do
@@ -62,75 +111,42 @@ matchCurrent chan hs = do
             return ()
         else do
             m <- readChan chan
-            matched <- match m Nothing hs
-            case matched of
-                Just False -> do
-                    matchCurrent chan hs
+            matched <- match m hs
+            if matched
+                then
+                    return ()
+                else do
+                    matchAll chan hs
                     unGetChan chan m
-                Just True ->
-                    return ()
-                Nothing ->
-                    error "Timed out even if no timeout given. This should not happen!"
 
-matchAll :: Chan m -> Maybe UTCTime -> [MsgHandler m] -> IO ()
-matchAll chan Nothing hs = do
-    m <- readChan chan
-    matched <- match m Nothing hs
-    case matched of
-        Just False -> do
-            matchAll chan Nothing hs
-            unGetChan chan m
-        Just True ->
-            return ()
-        Nothing ->
-            error "Timed out even if no timeout given. This should not happen!"
-matchAll chan (Just endTime) hs = do
-    curTime <- getCurrentTime
-    if curTime >= endTime
-        then return ()
-        else do
-            let to = round $ (diffUTCTime endTime curTime) * 1000000
-            mm <- timeout to $ readChan chan
-
-            case mm of
-                Just m -> do
-                    matched <- match m (Just endTime) hs
-                    case matched of
-                        Just False -> do
-                            matchAll chan (Just endTime) hs
-                            unGetChan chan m
-                        Just True ->
-                            return ()
-                        Nothing ->
-                            unGetChan chan m
-                Nothing ->
-                    return ()
-
-match :: m -> Maybe UTCTime -> [MsgHandler m] -> IO (Maybe Bool)
-match _ _ [] = return $ Just False
-match m Nothing (h : hs) = do
+match :: m -> [MsgHandler m] -> IO Bool
+match _ [] = return False
+match m (h : hs) = do
     ma <- catch (case h m of ((), a) -> return $ Just a)
                 handlePatternMatchFail
     case ma of
         Just action -> do
             action
-            return $ Just True
+            return True
         Nothing ->
-            match m Nothing hs
-match m (Just endTime) (h : hs) = do
-    curTime <- getCurrentTime
-    if curTime >= endTime
+            match m hs
+
+matchTimeout :: m -> UTCTime -> [MsgHandler m] -> IO (Maybe Bool)
+matchTimeout _ _ [] = return $ Just False
+matchTimeout m endTime (h : hs) = do
+    timeLeft <- calcTimeLeft endTime
+    if timeLeft <= 0
         then return Nothing
         else do
-            let to = round $ (diffUTCTime endTime curTime) * 1000000
-            ma <- timeout to $ catch (case h m of ((), a) -> return $ Just a)
-                                     handlePatternMatchFail
+            ma <- timeout timeLeft $
+                    catch (case h m of ((), a) -> return $ Just a)
+                          handlePatternMatchFail
             case ma of
                 Just (Just action) -> do
                     action
                     return $ Just True
                 Just Nothing ->
-                    match m (Just endTime) hs
+                    matchTimeout m endTime hs
                 Nothing -> do
                     return Nothing
 
