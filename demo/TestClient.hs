0c7319ec35b8f00d4ca63133434fb87a43e9f9d2
Andreas Baldeau <andreas@baldeau.net>
2010-05-17 23:40:23 +0200

    Updated demo to use (<!) and WrapBox.

--- demo/TestClient.hs
+++ demo/TestClient.hs
@@ -14,20 +14,21 @@ main = do
     hPutStr hdl "Test123\n"
     hFlush hdl
 
-    (inBox, _)  <- wrapReadHandle hdl
-                        (\inBox e -> inBox ! (error $ "Handled: " ++ show e))
-    (outBox, _) <- wrapWriteHandle hdl
-                        (\_ e -> inBox ! (error $ "Handled: " ++ show e))
+    inBox <- wrapReadHandle hdl
+                 (\inBox e -> inBox <! (error $ "Handled: " ++ show e))
+    outBox <- wrapWriteHandle hdl
+                 (\_ e -> inBox <! (error $ "Handled: " ++ show e))
 
     loop inBox outBox 1
 
-loop :: Mailbox Message -> Mailbox Message -> Int -> IO ()
-loop _inBox outBox 1000000 = outBox ! MsgCommand CmdQuit
+loop :: WrapBox Message -> WrapBox Message -> Int -> IO ()
+loop _inBox outBox 1000000 = outBox <! MsgCommand CmdQuit
 loop inBox outBox n = do
-    outBox ! M n
+    outBox <! M n
 
     receive inBox
         [ \m -> (#) $ do
             print m
             loop inBox outBox (n + 1)
         ]
+
--- demo/TestServer.hs
+++ demo/TestServer.hs
@@ -11,24 +11,24 @@ main = do
     sock <- listenOn $ UnixSocket "test.socket"
     (hdl, _, _) <- accept sock
 
-    (inBox, _)  <- wrapReadHandle hdl
-                        (\inBox e -> inBox ! (error $ "Handled: " ++ show e))
-    (outBox, _) <- wrapWriteHandle hdl
-                        (\_ e -> inBox ! (error $ "Handled: " ++ show e))
+    inBox <- wrapReadHandle hdl
+                 (\inBox e -> inBox <! (error $ "Handled: " ++ show e))
+    outBox <- wrapWriteHandle hdl
+                 (\_ e -> inBox <! (error $ "Handled: " ++ show e))
 
     loop inBox outBox
 
-loop :: Mailbox Message -> Mailbox Message -> IO ()
+loop :: WrapBox Message -> WrapBox Message -> IO ()
 loop inBox outBox = do
     receiveNonBlocking inBox
         [ \(MsgCommand CmdQuit) -> (#) $ return ()
         , \m -> (#) $ do
             putStrLn $ "Matched " ++ show m ++ " non-blocking."
-            outBox ! M (-1)
+            outBox <! M (-1)
             loop inBox outBox
         ] $ receive inBox
                 [ \(M (n + 1)) -> (#) $ do
-                    outBox ! M (n * 2)
+                    outBox <! M (n * 2)
                     loop inBox outBox
                 , \m -> (#) $ do
                     print m
