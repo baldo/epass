3810194f431e5f0a36e80dfba0d62e48336e083d
Andreas Baldeau <andreas@baldeau.net>
2010-10-01 15:03:49 +0200

    Cleanup and added docs.

--- src/Control/Concurrent/Mailbox/Wrapper.hs
+++ src/Control/Concurrent/Mailbox/Wrapper.hs
@@ -1,9 +1,15 @@
+-- | This module provides a wrapping mechanism for file handles (e.g. sockets)
 module Control.Concurrent.Mailbox.Wrapper
-    ( Wrappable (..)
+    (
+    -- * Wrapping types
+      Wrappable (..)
     , WrapBox
+    , ErrorHandler
 
+    -- * Wrapping functions
     , wrapReadHandle
     , wrapWriteHandle
+
     , wrapReadHandleWithMailbox
     , wrapWriteHandleWithMailbox
 
@@ -16,24 +22,38 @@ import Control.Concurrent.Mailbox
 import Control.Concurrent
 import System.IO
 
-type ErrorHandler m = Mailbox m -> IOError -> IO ()
-type Wrapper m = (Handle -> Mailbox m -> ErrorHandler m -> IO ())
 
+-- Wrapping types --------------------------------------------------------------
+
+{- | Messages send over wrapped handles must be instance of this class.
+
+     You only need to implement either 'fromString' or 'fromStringReadS'.
+-}
 class Wrappable m where
-    toString :: m -> String
+    -- | Convert a message to a 'String'
+    toString
+        :: m      -- ^ the message
+        -> String -- ^ its 'String' representation
 
-    fromString :: String -> Maybe m
+    -- | Convert a 'String' to the represented message.
+    fromString
+        :: String  -- ^ 'String' representation of a message
+        -> Maybe m -- ^ 'Just' the message or 'Nothing' in case of parse error
     fromString str =
         case fromStringReadS str of
             [(msg, "")] -> Just msg
             _           -> Nothing
 
+    -- | Same as 'fromString' but using 'ReadS' type.
     fromStringReadS :: ReadS m
     fromStringReadS str =
         case fromString str of
             Just msg -> [(msg, "")]
             Nothing  -> []
 
+{- | Wrapper around 'Mailbox'. For now the only 'MailboxClass' instance allowed
+     for wrapping.
+-}
 data WrapBox m = WBox { mBox :: Mailbox m, tId :: ThreadId }
 
 instance MailboxClass WrapBox where
@@ -42,19 +62,66 @@ instance MailboxClass WrapBox where
     putMessage   = putMessage   . mBox
     isEmpty      = isEmpty      . mBox
 
+{- | Function to be called in case of error. 'WrapBox' is the mailbox the error
+     occured on.
+-}
+type ErrorHandler m = WrapBox m -> IOError -> IO ()
+
+
+-- Wrapping functions ----------------------------------------------------------
+
+{- | Wrap the given 'Handle' for reading. The returned 'WrapBox' can be used
+     to receive messages from the 'Handle'.
+
+     Notice: The 'ErrorHandler' will be given the returned 'WrapBox'. Writing to
+     may not be what you want to do. Instead you might first call
+     'wrapWriteHandle' and then use its 'WrapBox' in 'wrapReadHandle's
+     'ErrorHandler'.
+-}
 wrapReadHandle
     :: Wrappable m
-    => Handle
-    -> ErrorHandler m
-    -> IO (WrapBox m)
+    => Handle         -- ^ handle to wrap
+    -> ErrorHandler m -- ^ error handler
+    -> IO (WrapBox m) -- ^ the wrapped mailbox
 wrapReadHandle = wrapHandle inWrapper
 
+{- | Wrap the given 'Handle' for writing. The returned 'WrapBox' can be used to
+     send messages through the 'Handle'.
+-}
 wrapWriteHandle
     :: Wrappable m
-    => Handle
-    -> ErrorHandler m
-    -> IO (WrapBox m)
+    => Handle         -- ^ handle to wrap
+    -> ErrorHandler m -- ^ error handler
+    -> IO (WrapBox m) -- ^ the wrapped mailbox
 wrapWriteHandle = wrapHandle outWrapper
+
+-- | Same as 'wrapReadHandle' but use an existing 'Mailbox' for wrapping.
+wrapReadHandleWithMailbox
+    :: Wrappable m
+    => Handle         -- ^ the handle to wrap
+    -> Mailbox m      -- ^ the mailbox to use
+    -> ErrorHandler m -- ^ error handler
+    -> IO (WrapBox m) -- ^ the wrapped mailbox
+wrapReadHandleWithMailbox = wrapHandleWithMailbox inWrapper
+
+-- | Same as 'wrapWriteHandle' but use an existing 'Mailbox' for wrapping.
+wrapWriteHandleWithMailbox
+    :: Wrappable m
+    => Handle         -- ^ the handle to wrap
+    -> Mailbox m      -- ^ the mailbox to use
+    -> ErrorHandler m -- ^ error handler
+    -> IO (WrapBox m) -- ^ the wrapped mailbox
+wrapWriteHandleWithMailbox = wrapHandleWithMailbox outWrapper
+
+-- | Cleanup the given 'WrapBox'. 
+closeWrapper
+    :: WrapBox m
+    -> IO ()
+closeWrapper = killThread . tId
+
+-- Wrapping (internal) ---------------------------------------------------------
+
+type Wrapper m = (Handle -> Mailbox m -> ErrorHandler m -> IO ())
 
 wrapHandle
     :: Wrappable m
@@ -67,22 +134,6 @@ wrapHandle wrapper hdl errHandler = do
     tid <- forkIO $ wrapper hdl mbox errHandler
     return WBox { mBox = mbox, tId = tid }
 
-wrapReadHandleWithMailbox
-    :: Wrappable m
-    => Handle
-    -> Mailbox m
-    -> ErrorHandler m
-    -> IO (WrapBox m)
-wrapReadHandleWithMailbox = wrapHandleWithMailbox inWrapper
-
-wrapWriteHandleWithMailbox
-    :: Wrappable m
-    => Handle
-    -> Mailbox m
-    -> ErrorHandler m
-    -> IO (WrapBox m)
-wrapWriteHandleWithMailbox = wrapHandleWithMailbox outWrapper
-
 wrapHandleWithMailbox
     :: Wrappable m
     => Wrapper m
@@ -93,11 +144,6 @@ wrapHandleWithMailbox
 wrapHandleWithMailbox wrapper hdl mbox errHandler = do
     tid <- forkIO $ wrapper hdl mbox errHandler
     return WBox { mBox = mbox, tId = tid }
-
-closeWrapper
-    :: WrapBox m
-    -> IO ()
-closeWrapper = killThread . tId
 
 inWrapper
     :: Wrappable m
@@ -112,8 +158,9 @@ inWrapper hdl mbox errHandler = do
                 Nothing  -> putStrLn $ "Error: Cannot parse message: " ++ show line
 
             inWrapper hdl mbox errHandler
-        Right e ->
-            errHandler mbox e
+        Right e -> do
+            tid <- myThreadId
+            errHandler WBox { mBox = mbox, tId = tid } e
 
 outWrapper
     :: Wrappable m
@@ -132,6 +179,8 @@ outWrapper hdl mbox errHandler = do
 
             case me of
                 Nothing -> outWrapper hdl mbox errHandler
-                Just e  -> errHandler mbox e
+                Just e  -> do
+                    tid <- myThreadId
+                    errHandler WBox { mBox = mbox, tId = tid } e
         ]
 
